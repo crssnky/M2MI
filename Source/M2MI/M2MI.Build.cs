e70aad73a411467ec33b04e37d86841a1ca38213
crssnky <vindicare28@yahoo.co.jp>
2020-05-21 10:40:29 +0900

    first

---
+++ Source/M2MI/M2MI.Build.cs
@@ -0,0 +1,58 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+using System.IO;
+using System;
+using UnrealBuildTool;
+
+namespace UnrealBuildTool.Rules
+{
+	public class M2MI : ModuleRules
+	{
+		public M2MI(ReadOnlyTargetRules Target) : base(Target)
+		{
+			PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+			PublicIncludePaths.AddRange(
+				new string[] {
+					// ... add public include paths required here ...
+				}
+				);
+
+			PrivateIncludePaths.AddRange(
+				new string[] {
+					// ... add other private include paths required here ...
+				}
+				);
+
+			PublicDependencyModuleNames.AddRange(
+				new string[]
+				{
+					"Core",
+					"UnrealEd",
+					"LevelEditor",
+					"Slate",
+					"SlateCore",
+					"MainFrame",
+					"InputCore",
+					// ... add other public dependencies that you statically link with here ...
+				}
+				);
+
+			PrivateDependencyModuleNames.AddRange(
+				new string[]
+				{
+					// ... add private dependencies that you statically link with here ...
+				}
+				);
+
+			DynamicallyLoadedModuleNames.AddRange(
+				new string[]
+				{
+					// ... add any modules that your module loads dynamically here ...
+				}
+				);
+		}
+
+		private string ModulePath {
+			get { return ModuleDirectory; }
+		}
+	}
+}
---
+++ Source/M2MI/Private/M2MI.cpp
@@ -0,0 +1,27 @@
+﻿// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "M2MI.h"
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+
+#define LOCTEXT_NAMESPACE "crssnkyPlugin"
+
+class FM2MI: public IM2MI{
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
+
+IMPLEMENT_MODULE(FM2MI, M2MI)
+
+void FM2MI::StartupModule(){
+	// This code will execute after your module is loaded into memory (but after global variables are initialized, of course.)
+	UE_LOG(LogTemp, Display, TEXT("Loaded GeoTIFF_Importer"));
+	if(IsRunningCommandlet()){ return; }
+}
+
+void FM2MI::ShutdownModule(){
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+	UE_LOG(LogTemp, Display, TEXT("Unloaded GeoTIFF_Importer"));
+}
