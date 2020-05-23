d847f8870caa3a35808518113d15613c771ca88d
crssnky <vindicare28@yahoo.co.jp>
2020-05-23 13:29:03 +0900

    fix ModuleName in UE_LOG & Be able to contain content

--- Source/M2MI/Private/M2MI.cpp
+++ Source/M2MI/Private/M2MI.cpp
@@ -16,12 +16,12 @@ IMPLEMENT_MODULE(FM2MI, M2MI)
 
 void FM2MI::StartupModule(){
 	// This code will execute after your module is loaded into memory (but after global variables are initialized, of course.)
-	UE_LOG(LogTemp, Display, TEXT("Loaded GeoTIFF_Importer"));
+	UE_LOG(LogTemp, Display, TEXT("Loaded M2MI"));
 	if(IsRunningCommandlet()){ return; }
 }
 
 void FM2MI::ShutdownModule(){
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
-	UE_LOG(LogTemp, Display, TEXT("Unloaded GeoTIFF_Importer"));
+	UE_LOG(LogTemp, Display, TEXT("Unloaded M2MI"));
 }
